897d869e0835069ff214cbd07f73d0ee0947acd6
Michael Peterson <michaeljoepeterson@gmail.com>
2019-12-15 11:53:23 -0700

    added api to service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,6 +3,7 @@ import {Hero} from './heroes/hero';
 import {HEROES} from './mock-heroes';
 import {MessageService} from './message.service';
 //used in http requests
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 import {Observable,of} from 'rxjs';
 //works dependency injection system
@@ -15,18 +16,23 @@ import {Observable,of} from 'rxjs';
   providedIn: 'root'
 })
 export class HeroService {
+  private heroesUrl = 'https://localhost:44328/api/hero';
   //this would be like the get action
   //return observable that returns a hero array
   //similar to c# tasks?
   getHeroes():Observable<Hero[]>{
     this.messageService.add('HeroService: fetched heros');
     //returns the observable which returns single value the hero array
-    return of(HEROES);
+    //return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
   getHero(id: number):Observable<Hero>{
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
   }
+  private log(message:string){
+    this.messageService.add('HeroService: ' + message);
+  }
   //inject the message service into this service which is then injected into the heroes component
-  constructor(private messageService:MessageService) { }
+  constructor(private messageService:MessageService, private http:HttpClient) { }
 }
